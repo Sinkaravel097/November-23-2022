193cdbf2e119a75c629f35569a1acdc595ca88a4
Sinkaravel097 <sinkaravel.career@gmail.com>
2022-11-25 09:06:52 +0530

    updated

--- src/com/test/EmployeeDetails.java
+++ src/com/test/EmployeeDetails.java
@@ -7,6 +7,9 @@ public class EmployeeDetails {
 		System.out.println(1);
 		System.out.println(2);
 		System.out.println(3);
+		System.out.println(10);
+		System.out.println(12);
+		System.out.println(30);
 		
 		
 	} 
